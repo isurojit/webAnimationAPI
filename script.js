bfa3530941fc35f4426dbdc7d34cfe2584709e2d
isurojit <iamsurojit@hotmail.com>
2023-09-21 12:08:02 +0530

    base File

---
+++ script.js
@@ -0,0 +1,6 @@
+const ball = document.getElementById("ball");
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+const reverse = document.getElementById("reverse");
+const speedUp = document.getElementById("speed-up");
+const slowDown = document.getElementById("slow-down");
