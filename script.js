292592dcf2e0a06ccdff19cac9edf0bccce6b571
isurojit <iamsurojit@hotmail.com>
2023-09-21 12:11:31 +0530

    Animation added

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const pause = document.getElementById("pause");
 const reverse = document.getElementById("reverse");
 const speedUp = document.getElementById("speed-up");
 const slowDown = document.getElementById("slow-down");
+
+const rollAnimation = [
+  {
+    transform: "rotate(0) translate3D(-50%, -50%, 0)",
+    color: "white",
+  },
+  {
+    color: "blue",
+    offset: 0.3,
+  },
+  {
+    transform: "rotate(360deg) translate3D(-50%, -50%, 0)",
+    color: "white",
+  },
+];
